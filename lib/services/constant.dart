ea6393cf8053cd569b72892745a526e00e6e96c1
vipul8815 <vipul@pietechsolution.com>
2020-04-25 11:42:53 +0530

    add url

--- lib/services/constant.dart
+++ lib/services/constant.dart
@@ -4,8 +4,13 @@ const APP_NAME = 'Restaurant Saas';
 //one signal
 const ONE_SIGNAL_APP_ID = '05ac6c51-8416-41ab-8779-b48503bbe125';
 
+// api deploy testing
+const BASE_URL = 'http://162.243.171.81:8000/';
+
+// api deploy production
+// const BASE_URL = 'http://162.243.171.81:7000/';
 //api urls
-const BASE_URL = 'https://restaurant-sass.herokuapp.com/';
+// const BASE_URL = 'https://restaurant-sass.herokuapp.com/';
 
 const API_ENDPOINT = BASE_URL + 'api/';
 
