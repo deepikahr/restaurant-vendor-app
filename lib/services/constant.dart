52343e1cfd945d88b811205d05a96fe93cae1f11
Sandip Kakadiya <sandip@pietechsolution.com>
2020-11-25 11:46:44 +0000

    Update constant.dart

--- lib/services/constant.dart
+++ lib/services/constant.dart
@@ -4,9 +4,10 @@ const APP_NAME = 'Restaurant Saas Vendor App';
 const ONE_SIGNAL_APP_ID = '05ac6c51-8416-41ab-8779-b48503bbe125';
 
 // api deploy testing
-//const BASE_URL = 'http://162.243.171.81:8000/';
+const BASE_URL = 'https://restaurant-saas-api-stg.ionicfirebaseapp.com/';
 
-const BASE_URL = 'http://162.243.171.81:8000/';
+// api deploy production
+//const BASE_URL = 'https://restaurant-saas-api.ionicfirebaseapp.com/';
 
 
 const API_ENDPOINT = BASE_URL + 'api/';
